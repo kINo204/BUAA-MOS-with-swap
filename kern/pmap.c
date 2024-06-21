3adca2b1c89a7097a3bae1284e3588290ff3a7f2
kINo <22371281@buaa.edu.cn>
2024-06-21 16:55:30 +0800

    new files merged

--- kern/pmap.c
+++ kern/pmap.c
@@ -4,6 +4,8 @@
 #include <mmu.h>
 #include <pmap.h>
 #include <swap.h>
+#include <types.h>
+#include <sdisk.h>
 #include <printk.h>
 
 /* These variables are set by mips_detect_memory(ram_low_size); */
@@ -550,4 +552,380 @@ void page_check(void) {
 	page_free(pa2page(PADDR(boot_pgdir)));
 
 	printk("page_check() succeeded!\n");
+}
+
+/* swap.c
+ */
+
+
+struct Page_tailq page_swap_queue;
+
+// Swap table: index table of PPage -> PTE
+//
+// `swap_tbl` is the starting addr of an SwapTableEntry array,
+// indexed by PPN.
+// Each SwapTableEntry is a LIST_HEAD item for a list of registered
+// VPage, whose info is recorded in struct SwapInfo.
+// When a VPage of swappable type is allocated with a PPage, a SwapInfo
+// will be registered through swap_register().
+// Later when trying to swap out the page, all corresponding SwapInfos
+// will be traversed to refill the page tables, unsetting V and setting
+// software flag SWAPPED, as well as writing disk address in the PTE.
+SwapTableEntry *swap_tbl;
+SwapTableEntry *bno_tbl;
+
+#define MAX_SWAPINFO 0x10000
+
+struct SwapInfo swapInfos[MAX_SWAPINFO];
+LIST_HEAD(, SwapInfo) swapInfo_free_list;
+
+void swap_init(void) {
+	// Init swap disk bitmap.
+	sd_bitmap_init();
+
+	// page_swap_queue
+	TAILQ_INIT(&page_swap_queue);
+
+	// swap_tbl, swapInfos, swapInfo_free_list
+	swap_tbl = (SwapTableEntry *)alloc(npage * sizeof(SwapTableEntry), PAGE_SIZE, 1);
+	bno_tbl = (SwapTableEntry *)alloc((SD_MAX / BLOCK_SIZE) * sizeof(SwapTableEntry), PAGE_SIZE, 1);
+	LIST_INIT(&swapInfo_free_list);
+	for (int i = 0; i < MAX_SWAPINFO; i++) {
+		LIST_INSERT_HEAD(&swapInfo_free_list, &swapInfos[i], link);
+	}
+}
+
+/* Register a SwapInfo in the corresponding ste in swap_tbl.
+   Called when a VPage of swappable type is accessed, requiring a PPage.
+ */
+void swap_register(struct Page *pp, Pde *pgdir, u_int va, u_int asid) {
+	// If it's the first time the PPage is mapped, insert it to swap_queue.
+	SwapTableEntry *pp_ste = page2ste(pp);
+	if (LIST_EMPTY(pp_ste)) {
+		TAILQ_INSERT_TAIL(&page_swap_queue, pp, swap_link); // Add the PPage to swappable queue.
+	}
+
+	// Get a free SwapInfo and edit it.
+	if (LIST_EMPTY(&swapInfo_free_list)) { panic("no struct SwapInfo available"); }
+	struct SwapInfo *sinfo = LIST_FIRST(&swapInfo_free_list);
+	LIST_REMOVE(sinfo, link);
+	sinfo->pgdir = pgdir;
+	sinfo->va    = va;
+	sinfo->asid  = asid;
+
+	// Insert sinfo to corresponding list of pp.
+	SwapTableEntry *ste = page2ste(pp);
+	LIST_INSERT_HEAD(ste, sinfo, link);
+}
+
+void swap_unregister(struct Page *pp, Pde *pgdir, u_int va, u_int asid) {
+	// Find the corresponding SwapInfo from the swap_tbl and remove it.
+	struct SwapInfo *sinfo;
+	SwapTableEntry *ste = page2ste(pp);
+	LIST_FOREACH(sinfo, ste, link) {
+		if ((((u_int) sinfo->pgdir) == ((u_int) pgdir))
+				&& (sinfo->asid == asid)
+				&& (PTE_ADDR(sinfo->va) == PTE_ADDR(va))) {
+			LIST_REMOVE(sinfo, link);
+			LIST_INSERT_HEAD(&swapInfo_free_list, sinfo, link);
+			break;
+		}
+	}
+
+	if (LIST_EMPTY(ste)) {
+		if (pp->swap_link.tqe_next) {
+			TAILQ_REMOVE(&page_swap_queue, pp, swap_link); // Remove the PPage from swappable queue.
+			pp->swap_link.tqe_next = NULL;
+			pp->swap_link.tqe_prev = NULL;
+		}
+	}
+}
+
+void swap_back(Pte cur_pte) {
+	//printk("back\n");
+	// Allocate a page.
+	struct Page *p;
+	panic_on(page_alloc(&p));
+
+	// Recover swapped data from the disk block.
+	u_int sd_bno = cur_pte >> PGSHIFT;
+	panic_on(sd_bno >= 0xfffff);
+	read_page(p, sd_bno);
+	sd_block_free(sd_bno);
+
+	// For all VPage of this swapped page, recover:
+	SwapTableEntry *bno_ste = bno2ste(sd_bno);
+	struct SwapInfo *sinfo;
+	LIST_FOREACH(sinfo, bno_ste, link) {
+		// Recover PTE.
+		Pde pde = sinfo->pgdir[PDX(sinfo->va)];
+		Pte *pte = (Pte *)KADDR( PTE_ADDR(pde) ) + PTX(sinfo->va);
+
+		*pte |= PTE_V;
+		*pte &= ~PTE_SWAPPED;
+		*pte = PTE_FLAGS(*pte);
+		*pte |= PTE_ADDR(page2pa(p));
+
+		p->pp_ref++;
+	}
+	TAILQ_INSERT_TAIL(&page_swap_queue, p, swap_link);
+
+	// Move SwapInfo from bno_ste to page_ste.
+	SwapTableEntry *ste = page2ste(p);
+	while (!LIST_EMPTY(bno_ste)) {
+		sinfo = LIST_FIRST(bno_ste);
+		LIST_REMOVE(sinfo, link);
+		LIST_INSERT_HEAD(ste, sinfo, link);
+	}
+	//printk("end back\n");
+}
+
+// Pick a page in memory and swap it out to the swapping disk.
+void swap(void) {
+	//printk("swap\n");
+
+	// Get a PPage from page_swap_queue.
+	if (TAILQ_EMPTY(&page_swap_queue)) { panic("no swappable page"); }
+	struct Page *pp = TAILQ_FIRST(&page_swap_queue);
+
+	// Write PPage data to a disk block.
+	int sd_bno = sd_block_alloc();
+	panic_on(sd_bno >= SD_NBLK);
+	write_page(pp, sd_bno);
+	//printk("page #0x%d data swapped to disk\n", page2ppn(pp));
+
+	// Refresh the PTE of all VPage mapping the swapped PPage and flush all TLB entries.
+	struct SwapInfo *sinfo;
+	SwapTableEntry *ste = page2ste(pp);
+	LIST_FOREACH(sinfo, ste, link){
+		Pde pde = sinfo->pgdir[PDX(sinfo->va)];
+		Pte *pte = (Pte *)KADDR( PTE_ADDR(pde) ) + PTX(sinfo->va);
+
+		//panic_on(!(*pte & PTE_V));
+		panic_on(*pte & PTE_SWAPPED);
+
+		*pte &= ~PTE_V;  	 		// Unset V.
+		*pte |= PTE_SWAPPED; 		// Set soft-flag SWAPPED.
+		*pte = PTE_FLAGS(*pte); 	// Clear PTE's PAddr field.
+		*pte |= PTE_ADDR(sd_bno << PGSHIFT); // Set addr to sd_bno.
+		tlb_invalidate(sinfo->asid, sinfo->va); // Invalidate corresponding TLB entry.
+	}
+
+	// Move sinfos from swap_tbl to bno_tbl.
+	SwapTableEntry *bno_ste = bno2ste(sd_bno);
+	while (!LIST_EMPTY(ste)) {
+		sinfo = LIST_FIRST(ste);
+		LIST_REMOVE(sinfo, link);
+		LIST_INSERT_HEAD(bno_ste, sinfo, link);
+	}
+
+	// Free the PPage.
+	pp->pp_ref = 0;
+
+	TAILQ_REMOVE(&page_swap_queue, pp, swap_link);
+	pp->swap_link.tqe_next = NULL;
+	pp->swap_link.tqe_prev = NULL;
+
+	LIST_INSERT_HEAD(&page_free_list, pp, pp_link);
+
+	//printk("end swap\n");
+}
+
+
+/* sdisk.c
+ * Operations on swapping disk.
+ * This is a CPU friendly PIO SWAP driver.
+ */
+
+int sd_bitmap[SD_NBLK / 32 + 1]; // Bits unset on free.
+
+int sd_block_alloc() {
+	int bno;
+	for (bno = 0; bno < SD_NBLK; bno++) {
+		if ((sd_bitmap[bno / 32] & (1 << (bno % 32))) == 0) {
+			sd_bitmap[bno / 32] |= (1 << (bno % 32));
+			if (bno >= 0xfffff) {
+				panic("sd_bno too big");
+			}
+			return bno;
+		}
+	}
+	panic("swap disk out of space");
+}
+
+void sd_block_free(u_int bno) {
+	sd_bitmap[bno / 32] &= ~(1 << (bno % 32));
+}
+
+void sd_bitmap_init() {
+	for (int i = 0; i < SD_NBLK / 32 + 1; i++) {
+		sd_bitmap[i] = 0;
+	}
+}
+
+static int read_sd(u_int va, u_int pa, u_int len) {
+	// Check length.
+	if (len != 1 && len != 2 && len != 4) { return -E_INVAL; }
+	// Check pa.
+	int valid_pa = 0;
+	valid_pa |= (0x18000170 <= pa && pa + len <= 0x18000170 + 0x8)  ? 1 : 0; // swap disk
+	if (valid_pa == 0) { return -E_INVAL; };
+
+	// Perform write and return.
+	memcpy((void*)va, (void*)(KSEG1 + pa), (size_t)len);
+	return 0;
+}
+
+static int write_sd(u_int va, u_int pa, u_int len) {
+	// Check length, only byte, half-word and word.
+	if (len != 1 && len != 2 && len != 4) { return -E_INVAL; }
+	// Check pa.
+	int valid_pa = 0;
+	valid_pa |= (0x18000170 <= pa && pa + len <= 0x18000170 + 0x8)  ? 1 : 0; // swap disk
+	if (valid_pa == 0) { return -E_INVAL; };
+
+	// Perform write and return.
+	memcpy((void*)(KSEG1 + pa), (void*)va, (size_t)len);
+	return 0;
+}
+
+// TODO hard polling!
+static uint8_t wait_sd_ready() {
+	uint8_t flag;
+	while (1) {
+		panic_on(read_sd(&flag, MALTA_SWAP_STATUS, 1));
+		if ((flag & MALTA_SWAP_BUSY) == 0) {
+			break;
+		}
+	}
+	return flag;
+}
+
+void sd_read(u_int diskno, u_int secno, void *dst, u_int nsecs) {
+	uint8_t temp; // Alloc a main mem BUFFER on the stack.
+	// `offset` records the starting offset of the next sector to operate.
+	u_int offset = 0, max = nsecs + secno;
+	panic_on(diskno != 2);
+
+	while (secno < max) {
+		temp = wait_sd_ready();
+		// Step 1: Write the number of operating sectors to NSECT register
+		temp = 1;
+		panic_on(write_sd(&temp, MALTA_SWAP_NSECT, 1));
+		// Step 2: Write the 7:0 bits of sector number to LBAL register
+		temp = secno & 0xff;
+		panic_on(write_sd(&temp, MALTA_SWAP_LBAL, 1));
+		// Step 3: Write the 15:8 bits of sector number to LBAM register
+		temp = (secno >> 8) & 0xff;
+		panic_on(write_sd(&temp, MALTA_SWAP_LBAM, 1));
+		// Step 4: Write the 23:16 bits of sector number to LBAH register
+		temp = (secno >> 16) & 0xff;
+		panic_on(write_sd(&temp, MALTA_SWAP_LBAH, 1));
+		// Step 5: Write the 27:24 bits of sector number, addressing mode
+		// and diskno to DEVICE register
+		temp = ((secno >> 24) & 0x0f) | MALTA_SWAP_LBA | (diskno << 4);
+		panic_on(write_sd(&temp, MALTA_SWAP_DEVICE, 1));
+		// Step 6: Write the working mode to STATUS register
+		temp = MALTA_SWAP_CMD_PIO_READ;
+		panic_on(write_sd(&temp, MALTA_SWAP_STATUS, 1));
+
+		// Step 7: Wait until the SWAP is ready
+		temp = wait_sd_ready();
+		// Step 8: Read the data from device
+		for (int i = 0; i < SECT_SIZE / 4; i++) {
+			panic_on(read_sd(dst + offset + i * 4, MALTA_SWAP_DATA, 4));
+		}
+		// Step 9: Check SWAP status
+		panic_on(read_sd(&temp, MALTA_SWAP_STATUS, 1));
+
+		offset += SECT_SIZE;
+		secno += 1;
+	}
+}
+
+void sd_write(u_int diskno, u_int secno, void *src, u_int nsecs) {
+	uint8_t temp;
+	u_int offset = 0, max = nsecs + secno;
+	panic_on(diskno != 2);
+
+	// Write the sector in turn
+	while (secno < max) {
+		temp = wait_sd_ready();
+		// Step 1: Write the number of operating sectors to NSECT register
+		temp = 1;
+		panic_on(write_sd(&temp, MALTA_SWAP_NSECT, 1));
+		// Step 2: Write the 7:0 bits of sector number to LBAL register
+		temp = secno & 0xff;
+		panic_on(write_sd(&temp, MALTA_SWAP_LBAL, 1));
+		// Step 3: Write the 15:8 bits of sector number to LBAM register
+		temp = (secno >> 8) & 0xff;
+		panic_on(write_sd(&temp, MALTA_SWAP_LBAM, 1));
+		// Step 4: Write the 23:16 bits of sector number to LBAH register
+		temp = (secno >> 16) & 0xff;
+		panic_on(write_sd(&temp, MALTA_SWAP_LBAH, 1));
+		// Step 5: Write the 27:24 bits of sector number, addressing mode
+		// and diskno to DEVICE register
+		temp = ((secno >> 24) & 0x0f) | MALTA_SWAP_LBA | (diskno << 4);
+		panic_on(write_sd(&temp, MALTA_SWAP_DEVICE, 1));
+		// Step 6: Write the working mode to STATUS register
+		temp = MALTA_SWAP_CMD_PIO_WRITE;
+		panic_on(write_sd(&temp, MALTA_SWAP_STATUS, 1));
+		// Step 7: Wait until the SWAP is ready
+		temp = wait_sd_ready();
+		// Step 8: Write the data to device
+		for (int i = 0; i < SECT_SIZE / 4; i++) {
+			/* Exercise 5.3: Your code here. (9/9) */
+			panic_on(write_sd(src + offset + i * 4, MALTA_SWAP_DATA, 4));
+		}
+		// Step 9: Check SWAP status
+		panic_on(read_sd(&temp, MALTA_SWAP_STATUS, 1));
+
+		offset += SECT_SIZE;
+		secno += 1;
+	}
+}
+
+void write_page(struct Page *pp, u_int bno) {
+	u_long kva = page2kva(pp);
+	sd_write(2, bno * SECT2BLK, kva, SECT2BLK);
+}
+
+void read_page(struct Page *pp, u_int bno) {
+	u_long kva = page2kva(pp);
+	sd_read(2, bno * SECT2BLK, kva, SECT2BLK);
+}
+
+void test_sdisk() {
+	printk("Testing sdisk...\n");
+	int magic = 0x12345678;
+	struct Page *p;
+	u_long base;
+
+	// Fill a page with magic num.
+	page_alloc(&p);
+	base = page2kva(p);
+	for (u_long ofs = 0; ofs < PAGE_SIZE; ofs += 4) {
+		*((int *)(base + ofs)) = magic;
+	}
+	printk("Finish writing the page\n");
+
+	// Write the content of the page to block 5.
+	write_page(p, 5);
+	page_free(p); // Free the page.
+	printk("Finish storing the page\n");
+
+	// Reallocate a page and read the disk to check.
+	page_alloc(&p);
+	base = page2kva(p);
+	read_page(p, 5);
+	for (u_long ofs = 0; ofs < PAGE_SIZE; ofs += 4) {
+		int value = *((int *)(base + ofs));
+		if (value != magic) {
+			panic("sdisk test failed, wrong value read");
+		} //else {
+			//printk("read value: 0x%x\n", value);
+		//}
+	}
+	printk("Finish reading the page\n");
+	printk("Test sdisk finished!\n");
 }
