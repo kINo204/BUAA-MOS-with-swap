7c5ab0370b71e296cfda6d6874ff92b4881a0ca6
kINo <22371281@buaa.edu.cn>
2024-06-24 16:22:26 +0800

    Retry

--- include/sdisk.h
+++ include/sdisk.h
@@ -5,7 +5,7 @@
 #include <types.h>
 
 #define SECT_SIZE 512
-#define SD_NBLK 2048 /* 8M */
+#define SD_NBLK 4096 /* 8M */
 #define SD_MAX (SD_NBLK * BLOCK_SIZE)
 // Restriction: PA in PTE is 20 bits, max 0xFFFFF.
 #define SECT2BLK (BLOCK_SIZE / SECT_SIZE)
