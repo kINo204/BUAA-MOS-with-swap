25bc126768ac63df7286d6e7993e58226443923b
kINo <22371281@buaa.edu.cn>
2024-06-26 03:01:13 +0800

    Fs try fix #5

--- user/lib/pageref.c
+++ user/lib/pageref.c
@@ -22,7 +22,7 @@ int pageref(void *v) {
 	/* Step 2: Check the page table. */
 	//int tmp = *((int *)v); // Trigger swapping back
 	pte = vpt[VPN(v)];
-	sys_mem_map(0, v, 0, v, PTE_FLAGS(pte)); // Trigger swapping back
+	syscall_mem_map(0, v, 0, v, PTE_FLAGS(pte)); // Trigger swapping back
 	pte = vpt[VPN(v)];
 	if (!(pte & PTE_V)) { return 0; }
 	/* Step 3: Return the result. */
